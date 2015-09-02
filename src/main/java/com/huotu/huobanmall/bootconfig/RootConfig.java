32f431e10afc62040482ea739b91a5d766fa8089
lgh <106517651@qq.com>
2015-09-02 09:39:43 +0800

    test规则修改

--- src/main/java/com/huotu/huobanmall/bootconfig/RootConfig.java
+++ src/main/java/com/huotu/huobanmall/bootconfig/RootConfig.java
@@ -1,6 +1,7 @@
 package com.huotu.huobanmall.bootconfig;
 
 import com.huotu.huobanmall.model.app.AppGlobalModel;
+import org.luffy.lib.libspring.data.ClassicsRepositoryFactoryBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
@@ -11,13 +12,13 @@ import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
  * Created by Administrator on 2015/8/19.
  */
 @Configuration
-@ComponentScan(value = {"com.huotu.huobanmall.service.impl","com.huotu.common.service.impl"})
+@ComponentScan(value = {"com.huotu.huobanmall.service.impl", "com.huotu.common.service.impl"})
 @ImportResource(value = {"classpath:spring-jpa.xml"})
-@EnableJpaRepositories(value = { "com.huotu.huobanmall.repository"})
+@EnableJpaRepositories(value = {"com.huotu.huobanmall.repository"}, repositoryFactoryBeanClass = ClassicsRepositoryFactoryBean.class)
 public class RootConfig {
 
     @Bean
-    public AppGlobalModel appGlobalModel(){
+    public AppGlobalModel appGlobalModel() {
         return new AppGlobalModel();
     }
 
--- src/main/java/com/huotu/huobanmall/repository/OrderRepository.java
+++ src/main/java/com/huotu/huobanmall/repository/OrderRepository.java
@@ -1,24 +1,25 @@
 package com.huotu.huobanmall.repository;
 
 import com.huotu.huobanmall.entity.Order;
+import org.luffy.lib.libspring.data.ClassicsRepository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.util.Date;
 import java.util.List;
 
-import java.util.Date;
 
 /**
  * Created by shiliting on 2015/8/27.
  */
 
 @Repository
-public interface OrderRepository extends JpaRepository<Order, String>, JpaSpecificationExecutor<Order> {
+public interface OrderRepository extends JpaRepository<Order, String>, JpaSpecificationExecutor<Order>,ClassicsRepository<Order> {
     List<Order> findByMerchantId(Integer merchantId);
     List<Order> findByMerchantIdAndTimeGreaterThan(Integer merchantId,Date lastTime);
 
-    @Query(value = "select count(*) from Order order where order.time>=?1")
+    @Query(value = "select count(order) from Order order where order.time>=?1")
     Integer countByWeekOrMonth(Date startDate);
 }
