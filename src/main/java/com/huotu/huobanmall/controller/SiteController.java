7c5cad56c30d3a8c8e3f48ad6aa5e609cd53a9d8
lgh <106517651@qq.com>
2015-10-08 13:27:16 +0800

    主订单

--- src/main/java/com/huotu/huobanmall/controller/SiteController.java
+++ src/main/java/com/huotu/huobanmall/controller/SiteController.java
@@ -1,11 +1,14 @@
 package com.huotu.huobanmall.controller;
 
 import com.huotu.huobanmall.api.common.PublicParameterHolder;
+import com.huotu.huobanmall.concurrency.impl.SystemCountingImpl;
 import com.huotu.huobanmall.model.app.AppPublicModel;
 import com.huotu.huobanmall.service.OrderService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.text.ParseException;
 
 /**
  * web网页
@@ -13,19 +16,16 @@ import org.springframework.web.bind.annotation.RequestMapping;
  */
 
 @Controller
-@RequestMapping("/app")
+@RequestMapping("/web")
 public class SiteController {
-    public static final int PAGE_SIZE=10;
-    @Autowired
-    OrderService orderService;
 
 
-    @RequestMapping("/saleList")
-    public String saleList() {
-        AppPublicModel appPublicModel = PublicParameterHolder.getParameters();
-        appPublicModel.getCurrentUser().getId();
-        return "x";
+    private SystemCountingImpl systemCounting;
+
+    @RequestMapping("/init")
+    public String init() throws ParseException {
+
+        systemCounting.InitHistoryDayAndToday();
+        return "finished";
     }
-
-
 }
