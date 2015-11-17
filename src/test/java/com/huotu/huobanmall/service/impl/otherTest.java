268b2ed91b91791bb86bdb8cd27c51ec77ad288d
shiliting <861819089@qq.com>
2015-11-17 09:26:59 +0800

    modify little

--- src/test/java/com/huotu/huobanmall/service/impl/otherTest.java
+++ src/test/java/com/huotu/huobanmall/service/impl/otherTest.java
@@ -19,8 +19,6 @@ import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.test.context.web.WebAppConfiguration;
 import org.springframework.transaction.annotation.Transactional;
 
-import java.util.List;
-
 /**
  * Created by Administrator on 2015/9/11.
  */
@@ -33,7 +31,5 @@ public class otherTest extends WebTestBase {
 
     @Test
     public void test1() throws Exception {
-        List<String> list=null;
-        int[] k=new int[list.size()];
     }
 }
